927913c83e45ae6ff7e2f6edc695d32cac67eb99
anna-pesetskaya <pesetskaya.anna@gmail.com>
2024-08-08 18:23:39 +0300

    Update task_1.ts

--- lesson_12/task_1.ts
+++ lesson_12/task_1.ts
@@ -14,27 +14,27 @@
 // //     }
 // // ];
 
-// interface User {
-//   name: string;
-//   age: number;
-//   occupation: string;
-//   car?: string;
-//   children?: number;
-// }
+interface User {
+  name: string;
+  age: number;
+  occupation: string;
+  car?: string;
+  children?: number;
+}
 
-// const users: User[] = [
-//   {
-//     name: 'Max Mustermann',
-//     age: 25,
-//     occupation: 'Chimney sweep',
-//     car: 'VW'
-//   },
-//   {
-//     name: 'Kate Müller',
-//     age: 23,
-//     occupation: 'Astronaut',
-//     children: 2
-//   }
-// ];
+const users: User[] = [
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+    car: 'VW'
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+    children: 2
+  }
+];
 
-// console.log(users);
+console.log(users);
