da30b4c46d8529ac39ca593fb4662093a8251cd8
anna-pesetskaya <pesetskaya.anna@gmail.com>
2024-08-08 18:24:09 +0300

    Update task_2.ts

--- lesson_12/task_2.ts
+++ lesson_12/task_2.ts
@@ -25,50 +25,49 @@
 // //     }
 // // ];
 
-// interface User {
-//     name: string;
-//     age: number;
-//     occupation: string;
-// }
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
   
-// interface Admin {
-//     role: string;
-// }
+interface Admin {
+    role: string;
+}
 
-// type Person = {
-//     id: number; 
-//     name: string;
-//     age: number;
-//     role?: string;
-//     occupation?: string; 
-// };
+type Person = {
+    id: number; 
+    name: string;
+    age: number;
+    role?: string;
+    occupation?: string; 
+};
 
-// const persons: Person[] = [
-// {
-//     id: 1,
-//     name: 'Max Mustermann',
-//     age: 25,
-//     occupation: 'Chimney sweep'
-// },
-// {
-//     id: 2,
-//     name: 'Jane Doe',
-//     age: 32,
-//     role: 'Administrator'
-// },
-// {
-//     id: 3,
-//     name: 'Kate Müller',
-//     age: 23,
-//     occupation: 'Astronaut'
-// },
-// {
-//     id: 4,
-//     name: 'Bruce Willis',
-//     age: 64,
-//     role: 'World saver'
-// }
-// ];
+const persons: Person[] = [
+{
+    id: 1,
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep'
+},
+{
+    id: 2,
+    name: 'Jane Doe',
+    age: 32,
+    role: 'Administrator'
+},
+{
+    id: 3,
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut'
+},
+{
+    id: 4,
+    name: 'Bruce Willis',
+    age: 64,
+    role: 'World saver'
+}
+];
 
-// // Проверка типа массива
-// console.log(persons); // Выведет массив объектов, соответствующих интерфейсу Person
+console.log(persons); 
